918ffbd4f55ab305d17afb022f2d71cc5a60bccf
asgabrielsilva <gabrielalmeida290905@gmail.com>
2023-10-06 11:25:46 -0300

    Mudanças de css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import api from './plugins/axios'
-
-const moviesGenres = ref([])
-const TVGenres = ref([])
-
-onMounted(async () => {
-  let response = await api.get('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR')
-  moviesGenres.value = response.data.genres
-  response =  await api.get('https://api.themoviedb.org/3/genre/tv/list?language=pt-BR')
-  TVGenres.value = response.data.genres
-})
 </script>
 
 <template>
-  <h1>Gêneros de filmes</h1>
-  <ul>
-    <li v-for="genre in moviesGenres" :key="genre.id">
-      {{ genre.name }} 
-    </li>
-  </ul>
-  <hr /><hr />
-  <h1>Gêneros de programas de TV</h1>
-  <ul>
-    <li v-for="genre in TVGenres" :key="genre.id">
-      {{ genre.name }}
-    </li>
-  </ul>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+
 <style scoped>
 
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0;
+  column-gap: 2rem;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
 </style>
